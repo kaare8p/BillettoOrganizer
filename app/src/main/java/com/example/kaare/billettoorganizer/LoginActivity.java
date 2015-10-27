5c75790af46c402ff76df2037343bda8dcf3a170
kaare8p <kaare1994@gmail.com>
2015-10-27 18:23:31 +0100

    Initial commit

---
+++ app/src/main/java/com/example/kaare/billettoorganizer/LoginActivity.java
@@ -0,0 +1,49 @@
+package com.example.kaare.billettoorganizer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button loginButton;
+    private EditText email;
+    private EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        password = (EditText) findViewById(R.id.password);
+        loginButton.setOnClickListener(this);
+
+        password.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    loginButton.performClick();
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, ActiveEvents.class);
+        startActivity(intent);
+    }
+}
