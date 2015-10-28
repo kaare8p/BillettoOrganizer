292cad4b521277471c639d12eef7f34b302d93cb
kaare8p <kaare1994@gmail.com>
2015-10-28 16:04:27 +0100

    API fis!!!

--- app/src/main/java/com/example/kaare/billettoorganizer/LoginActivity.java
+++ app/src/main/java/com/example/kaare/billettoorganizer/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.kaare.billettoorganizer;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
@@ -13,11 +14,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText emailEditText;
@@ -44,6 +51,16 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void onClickLogin(View v) {
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            URL url = new URL("https://billetto.dk/api/v1/user/login");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+
         Intent intent = new Intent(this, ActiveEvents.class);
         startActivity(intent);
     }
@@ -56,6 +73,45 @@ public class LoginActivity extends AppCompatActivity {
         intent.putExtra("email", email);
         intent.putExtra("password", password);
         startActivity(intent);
+    }
+
+    private class checkLogin extends AsyncTask<URL, String, String> {
+
+        @Override
+        protected String doInBackground(URL... params) {
+            try {
+                URL url = params[0];
+                HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setRequestProperty("ORGANIZATION", "1");
+                conn.setRequestProperty("Accept-Language", "en");
+
+                String body = "{\"email\":\"test@gruppe2.dk\", \"password\":\"test12345\", \"device_id\":\"0001\"}";
+                conn.setDoOutput(true);
+                DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+
+                dos.writeBytes(body);
+                dos.flush();
+                dos.close();
+
+                System.out.println("Response Code: " + conn.getResponseCode());
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String line = "";
+                StringBuilder response = new StringBuilder();
+
+                while ((line = br.readLine()) != null) {
+                    response.append(line);
+                }
+                System.out.println(response.toString());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 }
 
